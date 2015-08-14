e0e0622821ee19627b49fd1b4de833d90dfce77c
Jeffrey Jacka <jtjacka@gmail.com>
2015-08-14 14:07:09 -0700

    Add Parse query to retrieve images

--- Photo Filters App/ParseStarterProject/ParseService.swift
+++ Photo Filters App/ParseStarterProject/ParseService.swift
@@ -17,7 +17,7 @@ class ParseService {
     let imageData = UIImagePNGRepresentation(image)
     let imageFile = PFFile(name: "testphoto.png", data: imageData)
     
-    var userPhoto = PFObject(className:"UserPhoto")
+    var userPhoto = PFObject(className:"Image")
     userPhoto["imageName"] = "TestPhoto!"
     userPhoto["imageFile"] = imageFile
     userPhoto["comment"] = comment
@@ -34,8 +34,31 @@ class ParseService {
     
   }
   
-  class func downloadImagesFromParse() {
+  class func downloadImagesFromParse(completion:(imagesFromParse : [UIImage]) -> Void) {
+    let query = PFQuery(className: "Image")
     
+    var imagesFromQuery : [UIImage] = []
+    
+    query.findObjectsInBackgroundWithBlock { (results, error) -> Void in
+      if let error = error {
+        println(error.localizedDescription)
+      } else if let images = results as? [PFObject] {
+        println("Images received: \(images.count)")
+        for image in images {
+          if let imageFile = image["image"] as? PFFile {
+            imageFile.getDataInBackgroundWithBlock({ (data, error) -> Void in
+              if let error = error {
+                println(error.localizedDescription)
+              } else if let data = data,
+                image = UIImage(data: data) {
+                  imagesFromQuery.append(image)
+              }
+            })
+          }
+        }
+        //Send Images Array Back to the View Controller
+        completion(imagesFromParse: imagesFromQuery)
+      }
+    }
   }
-  
 }
